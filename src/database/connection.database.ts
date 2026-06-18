604c3d6a7c4852a7f42bc1511d38d0e8e7675fad
MATHEUS DE SANTANA FARIA <matheus.faria18@senaisp.edu.br>
2026-06-18 11:13:52 -0300

    arrumando ssl bd

--- src/database/connection.database.ts
+++ src/database/connection.database.ts
@@ -22,7 +22,10 @@ class Database {
             port: EnvVar.DB_PORT,
             waitForConnections: true,
             connectionLimit: 100,
-            queueLimit: 0
+            queueLimit: 0,
+            ssl: {
+                rejectUnauthorized: false
+            }
         })
     }
 
@@ -41,7 +44,10 @@ export async function initializeDatabase(): Promise<void> {
             host: EnvVar.DB_HOST,
             user: EnvVar.DB_USER,
             password: EnvVar.DB_PASSWORD,
-            port: EnvVar.DB_PORT
+            port: EnvVar.DB_PORT,
+            ssl: {
+                rejectUnauthorized: false
+            }
         });
 
         const dbName = EnvVar.DB_DATABASE || 'atividade3_db';
@@ -50,44 +56,44 @@ export async function initializeDatabase(): Promise<void> {
         await tempConnection.query(`USE \`${dbName}\`;`);
 
         await tempConnection.query(`
-            CREATE TABLE IF NOT EXISTS categorias (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                descricao VARCHAR(255) NOT NULL,
-                ativo TINYINT(1) DEFAULT 1
-            );
-        `);
+                CREATE TABLE IF NOT EXISTS categorias (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    descricao VARCHAR(255) NOT NULL,
+                    ativo TINYINT(1) DEFAULT 1
+                );
+            `);
 
         await tempConnection.query(`
-            CREATE TABLE IF NOT EXISTS clientes (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                nome VARCHAR(150) NOT NULL,
-                email VARCHAR(150) NOT NULL,
-                cpf VARCHAR(14) NOT NULL UNIQUE,
-                endereco VARCHAR(255) NOT NULL,
-                data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-            );
-        `);
+                CREATE TABLE IF NOT EXISTS clientes (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    nome VARCHAR(150) NOT NULL,
+                    email VARCHAR(150) NOT NULL,
+                    cpf VARCHAR(14) NOT NULL UNIQUE,
+                    endereco VARCHAR(255) NOT NULL,
+                    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                );
+            `);
 
         await tempConnection.query(`
-            CREATE TABLE IF NOT EXISTS produtos (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                nomeProd VARCHAR(150) NOT NULL,
-                valor DECIMAL(10, 2) NOT NULL,
-                idCategoria INT NOT NULL,
-                FOREIGN KEY (idCategoria) REFERENCES categorias(id) ON DELETE CASCADE
-            );
-        `);
+                CREATE TABLE IF NOT EXISTS produtos (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    nomeProd VARCHAR(150) NOT NULL,
+                    valor DECIMAL(10, 2) NOT NULL,
+                    idCategoria INT NOT NULL,
+                    FOREIGN KEY (idCategoria) REFERENCES categorias(id) ON DELETE CASCADE
+                );
+            `);
 
         await tempConnection.query(`
-            CREATE TABLE IF NOT EXISTS vendedores (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                nome VARCHAR(150) NOT NULL,
-                email VARCHAR(150) NOT NULL,
-                id_pedido INT NOT NULL,
-                matricula VARCHAR(50) NOT NULL UNIQUE,
-                data_admissao DATE NOT NULL
-            );
-        `);
+                CREATE TABLE IF NOT EXISTS vendedores (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    nome VARCHAR(150) NOT NULL,
+                    email VARCHAR(150) NOT NULL,
+                    id_pedido INT NOT NULL,
+                    matricula VARCHAR(50) NOT NULL UNIQUE,
+                    data_admissao DATE NOT NULL
+                );
+            `);
 
         await tempConnection.end();
         console.log("Banco de dados e tabelas verificados/criados com sucesso.");
