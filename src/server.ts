19c1c9e2697b639337ca879a0cb8abc8203b96c9
MATHEUS DE SANTANA FARIA <matheus.faria18@senaisp.edu.br>
2026-06-18 10:28:20 -0300

    alterando server.ts e adicionando o initialize

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
 import 'dotenv/config';
 import express from 'express';
-import routes from './routes/routes.js';
 import { initializeDatabase } from './database/connection.database';
+import router from './routes/routes.js';
 
 
 const app = express();
 
 
 app.use(express.json());
-app.use('/', routes);
+app.use('/', router);
 
 
 initializeDatabase().then(() => {
